b5884b0c56dbe810826a0c0cfc58f70c330990c1
jason evans <jason@jasons-MacBook-Pro.local>
2015-11-30 18:50:51 -0500

    gitignore added

--- app/utils/api/venue-api.js
+++ app/utils/api/venue-api.js
@@ -1,20 +1,24 @@
-var Promise = require('when').Promise;
+//var Promise = require('when').Promise;
 var venueDb = require('../db/venue-db');
 var cache = require('../cache');
+var Promise = require("node-promise").Promise;
 
 exports.get = (url, token) => {
   console.log('venue api get.');
   console.log('is venueDb null' + venueDb);
-  
+  var promise = new Promise();
+
   var cached = cache.get(token, url);
   if(cached){
-    return Promise.resolve(cached);
+    return promise.resolve(cached);
   }
 
   venueDb.get().then(function(res) {
     cache.set(token, url, res.data);
-    return res.data;
+    return promise.resolve(res.data);
   });
+
+  return promise;
 };
 
 exports.post = (url, data, token) => {
